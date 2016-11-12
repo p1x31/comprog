c4361380e7c0e662224f0bccaa05a1ed4063acc0
Vadim Makarenkov <Vadim Makarenkov>
2016-11-12 00:08:03 +0000

    added tamplate A+B and cf50

---
+++ cf50/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    void solve(Scanner in, PrintWriter out) {
+        int m = in.nextInt();
+        int n = in.nextInt();
+        out.println(m * n / 2);
+    }
+
+    void run() {
+        try (
+                Scanner in = new Scanner(System.in);
+                PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,25 @@ import java.util.*;
 
 public class Main {
 
-
     public static void main(String[] args) {
         new Main().run();
     }
 
     void solve(Scanner in, PrintWriter out) {
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int s = a + b;
+        out.println(s);
     }
 
     void run() {
         try (
-                Scanner in = new Scanner(System.in);
-                PrintWriter out = new PrintWriter(System.out);) {
+                Scanner in = new Scanner(new FileReader("sum.in"));//new Scanner(System.in);
+                PrintWriter out = new PrintWriter(new FileWriter("sum.out")))
+        {//new PrintWriter(System.out);) {
             solve(in, out);
+        }catch(IOException e){
+            throw new Error(e);
         }
     }
 }
